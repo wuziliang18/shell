b25a66ec58eb443a7ddbfd5581292c39ed5791c4
ziliang.wu <ziliang.wu@wuage.com>
2018-12-28 11:38:06 +0800

    提交

--- extractMessageContent.sh
+++ extractMessageContent.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-source_file="/cygdrive/d/测试数据/聊天记录钢和买.csv"
+source_file="/cygdrive/d/测试数.csv"
 #sed '1d' $source_file |cut  -d , -f 7,7 | sed  '/"{"/d'| sed  '/"本公司/d' |sort|uniq  >outFile
 cat $source_file |cut  -d , -f 1,1 |sort|uniq  >outFile
